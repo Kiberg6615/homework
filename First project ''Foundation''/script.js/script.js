ddf118eba61e39e0448b08aad9126e9c7d2f0160
Kiberg6615 <114940429+Kiberg6615@users.noreply.github.com>
2023-02-20 18:24:14 +0200

    Add files via upload

--- First project ''Foundation''/script.js/script.js
+++ First project ''Foundation''/script.js/script.js
@@ -3,3 +3,9 @@ $('.about-items > div ').on('click', function(){
  $(this).parent('.about-items') .toggleClass('change-color');
  $(this).next('.about-text').slideToggle(300);
 })
+
+$('.features-items > div ').on('click', function(){
+ $(this).children('.features-title') .toggleClass('title-changer');
+ $(this).parent('.features-items') .toggleClass('border-changer');
+ $(this).next('.features-text').slideToggle(300);
+})
